ca3a3ff41860f757f5c308bc3d8974815ccf6917
Denner Rubio <denner.psd@gmail.com>
2023-08-07 17:48:22 -0300

    update design header sobre

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,33 +4,41 @@ module.exports = {
   theme: {
     container: {
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1024px',
-        '2xl': '1024px',
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1024px",
+        "2xl": "1024px",
       },
     },
     extend: {
-      colors: {
-        'scooter': {
-          '50': '#eefafd',
-          '100': '#d4f2f9',
-          '200': '#aee4f3',
-          '300': '#76cfea',
-          '400': '#35b0d8',
-          '500': '#1c95be',
-          '600': '#1a77a0',
-          '700': '#1c6082',
-          '800': '#1f506b',
-          '900': '#1e435b',
-          '950': '#0e2b3e',
+      keyframes: {
+        flutuar: {
+          '0% , 100%': { transform: 'translateY(15px)' },
+          '50%': { transform: 'translateY(0px)' },
+        },
       },
+      animation: {
+        'flutuar': 'flutuar 3s ease-in-out infinite',
+      },
+      colors: {
+        scooter: {
+          50: "#eefafd",
+          100: "#d4f2f9",
+          200: "#aee4f3",
+          300: "#76cfea",
+          400: "#35b0d8",
+          500: "#1c95be",
+          600: "#1a77a0",
+          700: "#1c6082",
+          800: "#1f506b",
+          900: "#1e435b",
+          950: "#0e2b3e",
+        },
       },
       fontFamily: {
         notosans: ["Noto Sans", "sans-serif"],
       },
-      
     },
   },
   plugins: [],
