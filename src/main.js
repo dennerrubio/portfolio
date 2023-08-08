d5ab52d438d7ad3ddb243f56664a09078fc9c303
Denner Rubio <denner.psd@gmail.com>
2023-08-08 17:41:43 -0300

    organization sobre exp update

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ const botaoimg1 = document.querySelector("#botaoimg1");
 
 botao1.addEventListener("click", (e) => {
   document.querySelector("#botao1").disabled = true;
-  player1.src = "../assets/PLAYER1-ACTION.png";
-  player2.src = "../assets/PLAYER2-ACTION.png";
-  botaoimg1.src = "../assets/BOTAO1-CLICK.png";
+  player1.src = "../assets/art1/PLAYER1-ACTION.png";
+  player2.src = "../assets/art1/PLAYER2-ACTION.png";
+  botaoimg1.src = "../assets/art1/BOTAO1-CLICK.png";
   const timer = setInterval(function () {
-    player1.src = "../assets/PLAYER1-IDLE.png";
-    player2.src = "../assets/PLAYER2-IDLE.png";
-    botaoimg1.src = "../assets/BOTAO1-NORMAL.png";
+    player1.src = "../assets/art1/PLAYER1-IDLE.png";
+    player2.src = "../assets/art1/PLAYER2-IDLE.png";
+    botaoimg1.src = "../assets/art1/BOTAO1-NORMAL.png";
     document.querySelector("#botao1").disabled = false;
     clearInterval(timer);
   }, 1000);
